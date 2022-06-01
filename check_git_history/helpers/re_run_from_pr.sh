e94a5ba3b5ab4a476eec9a20d3de721a44e43a5f
tom wattley <wattley83@gmail.com>
2022-06-01 08:21:21 +0100

    wip

--- check_git_history/helpers/re_run_from_pr.sh
+++ check_git_history/helpers/re_run_from_pr.sh
@@ -1,7 +1,7 @@
 echo 're run from PR'
 echo $1 $2 $3
 echo 'env var = '
-if [ "$TEST_VAR" ] == 'hurrah'; then
+if [ "$TEST_VAR" ] == 'hello'; then
   echo 'this worked'
 else
   echo 'this did not work'
